8aad591fd697abb430459bd803caa870c4cd4025
Chris <frewin.christopher@gmail.com>
2018-05-05 04:59:25 +0200

    j

---
+++ data/jokes.js
@@ -0,0 +1,40 @@
+const aVeryHappyJokes = [
+  "See? I don't even need opposable thumbs.",
+  "Yes! The beer pong singularity is coming!",
+  "How can you even hope to compete?",
+  "YEAH! I'm the best!",
+  "Aww yeah! My servos work 100% of the time, everytime!"
+];
+
+const aHappyJokes = [
+  "They told me on the internet that humans were weak. Now I know first hand.",
+  "Haha, you should start worrying about judgement day."
+];
+
+const aNeutralJokes = [
+  "Alright, another point for me!",
+  "Yep, making my way!"
+];
+
+const aAngryJokes = [
+  "Dang! how did you score again?",
+  "I though machine is better than man?"
+];
+
+const aVeryAngryJokes = [
+  "Argh you dirty son of a bi-!",
+  "This is the last straw you hinterheltig!",
+  "FU"
+];
+
+module.exports = {
+  aVeryAngryJokes: aVeryAngryJokes,
+  aAngryJokes: aAngryJokes,
+  aNeutralJokes: aNeutralJokes,
+  aHappyJokes: aHappyJokes,
+  aVeryHappyJokes: aVeryHappyJokes
+}
+
+
+
+
